82822d7e4254b5b08f4e685a3d76a7f896e49ca5
shariqkhan27 <shariqmuzahir@gmail.com>
2026-09-22 10:29:50 +0500

    Fix PWA manifest criteria by adding 192x192 icon requirement

--- public/sw.js
+++ public/sw.js
@@ -5,6 +5,7 @@ const OFFLINE_URL = '/offline.html';
 const STATIC_ASSETS = [
   OFFLINE_URL,
   '/manifest.json',
+  '/icons/icon-192x192.png',
   '/icons/icon-512x512.png',
   '/icons/icon-maskable-512x512.png',
 ];
